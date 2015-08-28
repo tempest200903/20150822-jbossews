0e0328a503f4871707e158afebf32071000c277d
tempest200903 <tempest200903@gmail.com>
2015-08-28 17:11:41 +0900

    最低限、この3つがあれば Wicket は動く。

---
+++ src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+
+public class HelloWorld extends WebPage {
+    public HelloWorld() {
+        add(new Label("message", "Hello World!"));
+    }
+}
---
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.apache.wicket.protocol.http.WebApplication;
+
+public class HelloWorldApplication extends WebApplication {
+    public HelloWorldApplication() {
+    }
+    /**
+     * @see org.apache.wicket.Application#getHomePage()
+     */
+    @Override
+    public Class getHomePage() {
+        return HelloWorld.class;
+    }
+}
