9e33dfb87984fd7f585648cef5e8edc39336a636
tempest200903 <tempest200903@gmail.com>
2015-08-29 16:19:33 +0900

    HelloWorld

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,11 +1,12 @@
 package com.example;
 
+import java.util.Date;
+
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
 
 public class HelloWorld extends WebPage {
-    public HelloWorld() {
-        System.out.println("com.example.HelloWorld.HelloWorld()");
-        add(new Label("message", "Hello World!"));
-    }
+	public HelloWorld() {
+		System.out.println("com.example.HelloWorld " + new Date());
+		// add(new Label("message", "Hello World!"));
+	}
 }
