d141ecdfce02dbcea175839d34b76808270dc8c0
tempest200903 <tempest200903@gmail.com>
2015-08-29 13:19:14 +0900

    maven-jetty-plugin を用いる方法

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -5,6 +5,7 @@ import org.apache.wicket.markup.html.basic.Label;
 
 public class HelloWorld extends WebPage {
     public HelloWorld() {
+        System.out.println("com.example.HelloWorld.HelloWorld()");
         add(new Label("message", "Hello World!"));
     }
 }
--- src/main/java/com/example/HelloWorldApplication.java
+++ src/main/java/com/example/HelloWorldApplication.java
@@ -5,11 +5,13 @@ import org.apache.wicket.protocol.http.WebApplication;
 public class HelloWorldApplication extends WebApplication {
     public HelloWorldApplication() {
     }
+
     /**
      * @see org.apache.wicket.Application#getHomePage()
      */
     @Override
     public Class getHomePage() {
+        System.out.println("com.example.HelloWorldApplication.getHomePage()");
         return HelloWorld.class;
     }
 }
